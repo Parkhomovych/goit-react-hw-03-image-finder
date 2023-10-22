d463312a7cbc65cae033ecc71eaeb766a17a5a53
Vladyslav Parkhomovych <adamantinevlad@gmail.com>
2023-10-22 14:37:38 +0300

    Update PixabayAPI.js

--- src/components/PixabayAPI.js
+++ src/components/PixabayAPI.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const BASE_URL = 'https://pixabay.com/api?/';
+const BASE_URL = 'https://pixabay.com/api/?';
 const KEY = '38997661-54e537908498a57afa3a31c75';
 
 export const fetchImg = async (query, currentPage) => {
